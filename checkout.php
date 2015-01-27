ae0e72ef625de42d86957bff6efbd70b246471c8
Justin <me@justinmaslin.com>
2015-01-27 02:02:42 -0500

    Checkout page template

---
+++ checkout.php
@@ -0,0 +1,13 @@
+<?php include 'php/views/header.php'; ?>
+
+	<div id="content" class="row">
+
+		<?php include 'php/views/side.php'; ?>
+		
+		<div id="main" class="col-sm-9 col-xs-12">
+			<?php include 'public/logparts/new-loan.php'; ?>
+		</div>
+
+	</div>
+
+<?php include 'php/views/footer.php'; ?>
