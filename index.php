bc45e7a2ccd8b51d1fb9a25026ef7c2bcc00fbad
Justin Maslin <me@justinmaslin.com>
2015-01-27 17:58:05 -0500

    PHP index same base layout

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 	<div id="content" class="row">
 
 		<?php include 'php/views/side.php'; ?>
+		
+		<div id="main" class="col-sm-9 col-xs-12">
+			<?php include 'public/logparts/table.php'; ?>
+		</div>
 
 	</div>
 
