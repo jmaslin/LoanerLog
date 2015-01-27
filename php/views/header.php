6427b35eb5ad4362abfac44419ea5414c00f1eb7
Justin <me@justinmaslin.com>
2015-01-27 02:02:12 -0500

    Break into views

---
+++ php/views/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Loaner Log</title>
+		<!-- CSS -->
+		<link href="/components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<link href="/assets/css/main.css" rel="stylesheet" media="screen">
+		<link href="/assets/css/loantable.css" rel="stylesheet" media="screen">
+		<!-- JS -->
+		<script src="/components/jquery/dist/jquery.min.js"></script>
+		<script src="/components/bootstrap/dist/js/bootstrap.min.js"></script>
+		<link rel="icon" href="/favicon.ico" />
+		<!-- PickADate -->
+		<script src="/components/pickadate/lib/picker.js"></script>
+		<script src="/components/pickadate/lib/picker.date.js"></script>
+		<script src="/components/pickadate/lib/picker.time.js"></script>
+		<link href="/components/pickadate/lib/themes/classic.css" rel="stylesheet" media="screen"></script>
+		<link href="/components/pickadate/lib/themes/classic.date.css" rel="stylesheet" media="screen"></script>
+		<link href="/components/pickadate/lib/themes/classic.time.css" rel="stylesheet" media="screen"></script>
+		<!-- SweetAlert -->
+		<script src="/assets/js/sweet-alert.js"></script>
+		<link href="/components/bootstrap-sweetalert/lib/sweet-alert.css" rel="stylesheet" media="screen">
+		<!-- Moment -->
+		<script src="/components/moment/min/moment.min.js"></script>
+
+	</head>
+	<body>
+
+		<div class="container">
+
+			<!-- HEADER -->
+			<div id="head" class="row">			
+				<div class="col-sm-8">	
+					<h1 class="head-item">Loaner Log <a id="org-linkback" href="#"><small class="text-primary">Intranet</small></a></h1>
+				</div>
+				<div class="col-sm-4">
+					<div class="lead head-item user-login pull-right">
+						<p>Hello,</p> <a id="logged-in" href="#profile"><span id="user-logged-in" class="text-primary">Justin Maslin</span></a>&nbsp;
+						<button disabled type="button" class="btn btn-sm btn-info text-right">Logout</button>
+					</div>
+				</div>
+				<div class="col-md-12">
+					<div class="hr"><hr></div>	
+				</div>	
+			</div>
+			<!-- HEADER END -->
---
+++ php/views/side.php
@@ -0,0 +1,76 @@
+	<!-- SIDEBAR -->
+				<div id="side" class="col-sm-3">
+
+					<!-- TODO: Give each sidebar module its own box with shadow, pop-up from background. -->
+
+					<div id="loan-category" class="sidebar-module">
+						<div class="row">
+							<div class="col-sm-12">
+								<h3 class="text-primary text-center">Loan Actions</h3>
+								<hr>
+							</div>
+							<div class="col-sm-6 col-md-12">
+								<button type="button" class="btn btn-info loan-btn" data-toggle="modal" data-target="#loan-out-modal"><span class="lead">Check Out</span></button>
+							</div>
+							<div class="col-sm-6 col-md-12">
+								<button type="button" class="btn btn-success loan-btn" data-toggle="modal" data-target="#loan-in-modal"><span class="lead">Check In</span></button>
+							</div>
+						</div>
+					</div>
+
+					<div id="loan-inventory" class="sidebar-module">
+						<div class="row">
+							<h3 class="text-primary text-center">Available Inventory</h3>
+							<hr>
+						</div>
+						<div id="loan-inventory-categories" class="row text-center"> 
+						<!-- Each inventory category item gets own col-md-3 span, that way rollover looks good. Dynamically load from ajax request on available asset classes and inventories. IDs of main div (with class col-md-3) will have custom idents loaded. -->
+							<div id="loan-type-laptop1" class="inventory-item col-md-4 col-xs-3">
+								<img class="img img-responsive" src="/assets/img/laptop.png">
+								<span class="badge">12</span>
+							</div>
+							<div id="loan-type-mic" class="inventory-item col-md-4 col-xs-3">
+								<img class="img img-responsive" src="/assets/img/mic.png">
+								<span class="badge">4</span>
+							</div>
+							<div id="loan-type-remote" class="inventory-item col-md-4 col-xs-3">
+								<img class="img img-responsive" src="/assets/img/remote.png">
+								<span class="badge">2</span>
+							</div>	
+							<div id="loan-type-webcam" class="inventory-item col-md-4 col-xs-3">
+								<img class="img img-responsive" src="/assets/img/webcam.png">
+								<span class="badge">1</span>
+							</div>		
+							<div id="loan-type-hdmi-adapter" class="inventory-item col-md-4 col-xs-3">
+								<img class="img img-responsive" src="/assets/img/hdmi-adapter.png">
+								<span class="badge">4</span>
+							</div>	
+							<div id="loan-type-vga-adapter" class="inventory-item col-md-4 col-xs-3">
+								<img class="img img-responsive" src="/assets/img/camera.png">
+								<span class="badge">3</span>
+							</div>																												
+						</div>
+					<!-- Would show how many left / how many total. Example: Laptops (4/5) -->
+					</div>
+
+
+					<div id="loan-admin" class="sidebar-module">
+						<div class="row">
+							<h3 class="text-primary text-center">Admin Options</h3>
+							<hr>
+						</div>
+						<div class="row">
+							<ul id="admin-log-options" class="nav nav-stackable nav-pills">
+								<li role="presentation"><a href="#">Manage Assets</a></li>
+								<li role="presentation"><a href="#">Manage Users</a></li>
+								<li role="presentation"><a href="#"></a></li>
+							</ul>
+						</div>			
+					</div>
+<!-- 					<div class="loan-category">
+						<h4 title="Adapters, Mice, Speakers">Accessories</h4>
+						<button type="button" class="btn btn-primary loan-btn">Check Out</button>
+						<button type="button" class="btn btn-info loan-btn">Check In</button>						
+					</div> -->
+				</div>
+				<!-- SIDEBAR END -->
