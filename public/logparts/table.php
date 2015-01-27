e8955ab439af35330def978ddfe77887b5b5d0b1
Justin Maslin <me@justinmaslin.com>
2015-01-27 17:58:20 -0500

    Move static table to own class file

---
+++ public/logparts/table.php
@@ -0,0 +1,119 @@
+<div class="row">
+  <h3>Loan Table</h3>
+	<hr>
+</div>
+
+	<!-- LOANER TABLE -->
+	<div class="table-responsive">
+		<table id="loan-table" class="table table-hover table-bordered text-center">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<!-- <th>User Type</th> -->
+					<th class="hidden-xs">Details</th>
+					<th>Time</th>
+					<th>Due</th>
+					<th>Options</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr class="loan-row" data-loan-id="1">
+					<td class="user-td text-center">
+						<span class="user-name">John Fry</span>
+						<span class="user-type label label-info visible-xs-block visible-sm-block visible-md-inline visible-lg-inline">Student</span>
+					</td>
+					<td class="assets-td hidden-xs">
+						<div class="asset-preview more-info pull-right">
+							<img class="img img-responsive" src="/assets/img/info.png">
+						</div>										
+						<div class="asset-preview">
+							<img class="img img-responsive" src="/assets/img/laptop.png">
+							<span class="badge">14</span>
+						</div>
+						<div class="asset-preview">
+							<img class="img img-responsive" src="/assets/img/mic.png">
+						</div>
+						<div class="asset-preview">
+							<img class="img img-responsive" src="/assets/img/vga-adapter.png">
+						</div>
+					</td>
+					<td class="time-td">
+						<code class="table-info-date">12:42pm</code>
+					</td>
+					<td class="due-td">
+						<code class="table-info-date">Today</code>
+					</td>
+					<td class="options-td">
+						<button class="btn btn-xs btn-info visible-xs-block">Info</button>
+						<button class="check-in btn btn-xs btn-primary">Check In</button>
+					</td>
+				</tr>
+				<tr class="loan-row" data-loan-id="1">
+					<td class="user-td text-center">
+						<span class="user-name">John Fry</span>
+						<span class="user-type label label-info visible-xs-block visible-sm-block visible-md-inline visible-lg-inline">Student</span>
+					</td>
+					<td class="assets-td hidden-xs">
+						<div class="asset-preview more-info pull-right">
+							<img class="img img-responsive" src="/assets/img/info.png">
+						</div>										
+						<div class="asset-preview">
+							<img class="img img-responsive" src="/assets/img/laptop.png">
+							<span class="badge">14</span>
+						</div>
+						<div class="asset-preview">
+							<img class="img img-responsive" src="/assets/img/mic.png">
+						</div>
+						<div class="asset-preview">
+							<img class="img img-responsive" src="/assets/img/vga-adapter.png">
+						</div>
+					</td>
+					<td class="time-td">
+						<code class="table-info-date">12:42pm</code>
+					</td>
+					<td class="due-td">
+						<code class="table-info-date">Today</code>
+					</td>
+					<td class="options-td">
+						<button class="btn btn-xs btn-info visible-xs-block">Info</button>
+						<button class="check-in btn btn-xs btn-primary">Check In</button>
+					</td>
+				</tr>									
+<!-- 								
+				<tr class="table-loan-entry" data-loan-id="1">
+					<td class="table-info-user">
+						<span class="table-info-user-name">John Fry</span>
+						<span class="label label-info table-info-user-type visible-xs-block visible-sm-block visible-md-inline visible-lg-inline">Student</span>
+					</td>
+					<td class="table-loan-info hidden-xs">
+						<div class="table-loan-moreinfo pull-right">
+							<img class="img img-responsive img-line" src="/assets/img/info.png">
+						</div>
+						<div class="loan-info-asset">
+							<img class="img img-responsive img-line" src="/assets/img/laptop.png">
+							<span class="badge">14</span>
+						</div>
+						<div class="loan-info-asset">
+							<img class="img img-responsive img-line" src="/assets/img/mic.png">
+						</div>
+						<div class="loan-info-asset">
+							<img class="img img-responsive img-line" src="/assets/img/vga-adapter.png">
+						</div>		
+					</td>
+					<td class="table-date-row">
+						<code><span class="table-info-date">12:42pm</span></code>
+					</td>
+					<td>
+						<code><span class="table-info-date">Today</span></code>
+					</td>
+					<td class="table-button-group">
+						<button class="btn btn-xs btn-info check-in visible-xs-block">Info</button>
+						<button class="btn btn-xs btn-primary check-in">Check In</button>
+					</td>
+				</tr>		 
+-->				
+			</tbody>
+		</table>
+	</div>
+</div>
+<!-- LOANER TABLE END -->
