6b6ab4565f9d3dc6be1585af2b4f1b28f10a4988
Justin <me@justinmaslin.com>
2015-01-29 18:21:39 -0500

    Readd

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'php/views/header.php'; ?>
+
+	<div id="content" class="row">
+
+		<?php include 'php/views/side.php'; ?>
+		
+		<div id="main" class="col-md-9 col-xs-12">
+			<?php include 'public/logparts/table.php'; ?>
+		</div>
+
+	</div>
+
+<?php include 'php/views/footer.php'; ?>
